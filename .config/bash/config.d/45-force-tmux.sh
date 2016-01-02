c191739c1a8af1e9e3941062e7c6cef6161898e6
ek9 <dev@ek9.co>
2016-01-02 16:29:07 +0100

    force-tmux: session naming update

--- .config/bash/config.d/45-force-tmux.sh
+++ .config/bash/config.d/45-force-tmux.sh
@@ -3,5 +3,5 @@
 if which tmux 2>&1 >/dev/null; then
     test -z "$TMUX" && test -z "$GIT_DIR" && \
       (tmux ls | grep -vq attached && tmux -2 at \
-            || tmux -2 new-session -n$USER -s$USER@$HOSTNAME-`date +%s-%N`)
+            || tmux -2 new-session -s $USER-`date +%Y%m%d%H%M`)
 fi
