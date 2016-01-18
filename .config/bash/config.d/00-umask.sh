b717c78ac74f296f9d5bd458857b159ff958d353
ek9 <dev@ek9.co>
2016-01-18 23:59:10 +0100

    bash: umask script update

--- .config/bash/config.d/00-umask.sh
+++ .config/bash/config.d/00-umask.sh
@@ -1,1 +1,6 @@
+#!/usr/bin/env bash
+## ek9/dotfiles - https://github.com/ek9/dotfiles
+## 00-umask.sh
+## Set default umask to 027 - a good compromise between security and simplicity
+## https://blogs.gentoo.org/mgorny/2011/10/18/027-umask-a-compromise-between-security-and-simplicity/
 umask 027
