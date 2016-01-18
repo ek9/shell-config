b04e0862f770b7107441943ca1dbb6a7b24a10ee
ek9 <dev@ek9.co>
2016-01-19 00:01:14 +0100

    bash: force tmux script cleanup

--- .config/bash/config.d/05-force-tmux.sh
+++ .config/bash/config.d/05-force-tmux.sh
@@ -1,7 +1,9 @@
-# force TMUX use
-# (tries attaching to non-attached session before creating new one)
+#!/usr/bin/env bash
+## ek9/dotfiles - https://github.com/ek9/dotfiles
+## 05-force-tmux.sh
+## force opening tmux - tries attaching to non-attached session first
 if [[ -x $(command -v tmux) ]]; then
     test -z "$TMUX" && test -z "$VCSH_DIRECTORY" && \
       (tmux ls | grep -vq attached && tmux -2 at \
-            || tmux -2 new-session -s $USER-`date +%Y%m%d%H%M`)
+            || tmux -2 new-session -c 'tmux choose-session')
 fi
