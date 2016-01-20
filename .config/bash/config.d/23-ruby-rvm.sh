1904c5fc61b9ef618b075419c4745dc6349a8563
ek9 <dev@ek9.co>
2016-01-20 02:16:04 +0100

    bash: ruby rvm init cleanup

--- .config/bash/config.d/23-ruby-rvm.sh
+++ .config/bash/config.d/23-ruby-rvm.sh
@@ -1,9 +1,18 @@
-# add rvm to end of path
-#RVMPATH="$HOME/.rvm/bin"
-#if [[ $UID -ge 1000 && -d $RVMPATH && -z $(echo $PATH | grep -o $RVMPATH) ]]; then
-#    export PATH="$PATH:$RVMPATH" # Add RVM to PATH for scripting
-#fi
+#!/usr/bin/env bash
+## ek9/dotfiles - https://github.com/ek9/dotfiles
+## 23-ruby-rvm.sh
+## source RVM if it is installed
+if [[ -s "$HOME/.rvm/scripts/rvm" ]]; then
+    source "$HOME/.rvm/scripts/rvm"
+    export PATH="$HOME/.rvm/bin:$PATH" # Add RVM to PATH for scripting
+elif command -v gem >/dev/null; then
 
-# source RVM if it is installed
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+    # apppend user's gems to PATH if directory exists and it is not yet in PATH
+    GEMBIN="$(ruby -e 'print Gem.user_dir')/bin"
+    if [[ $UID -ge 1000 && -d $GEMBIN && -z $(echo $PATH | grep -o $GEMBIN) ]]
+    then
+        export PATH=$PATH:$GEMBIN
+        export GEM_HOME=$GEMBIN
+    fi
+fi
 
