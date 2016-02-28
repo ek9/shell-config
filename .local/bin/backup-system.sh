1e37d6088c6480f66ea141b4755b807f84a35ecc
ek9 <dev@ek9.co>
2016-02-28 01:14:44 +0100

    update backup-system.sh bin

--- .local/bin/backup-system.sh
+++ .local/bin/backup-system.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
+if [ -z $1 ] || [ ! -d "$1" ]; then
 	echo "Please provide target directory as an argument"
 	exit 1
 fi
 
-rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} --delete /* $1
+sudo rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} --delete /* $1
