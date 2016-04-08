4116faf6f3561578c54965338de16ee606a1ae7e
ek9 <dev@ek9.co>
2016-04-08 16:35:11 +0200

    bash: nodejs nvm/npm startup performance optimizations

--- .config/shell/config.d/22-nodejs.sh
+++ .config/shell/config.d/22-nodejs.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 ## ek9/dotfiles - https://github.com/ek9/dotfiles
-## 22-node-npm.sh
+## 22-nodejs.sh
 ## Configures node, with either nvm or npm global support (no sudo)
 
 NVM_DIR="${HOME}/.local/share/nvm"
 NPM_PACKAGES="${HOME}/.local/share/npm"
 # try to initialize nvm first
 if [[ $UID -ge 1000 && -d "$NVM_DIR" ]]; then
-    # load nvm
-    export NVM_DIR="${NVM_DIR}"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    nvm(){
+    if type nvm >/dev/null 2>&1 && [ -f "$NVM_DIR/nvm.sh" ] ; then
+        #if nvm not loaded
+        #echo "nvm init loading..."
+        [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && nvm "$@" #loads nvm
+    else
+        nvm "$@"
+    fi
+	}
 elif [[ $UID -ge 1000 && -d "$NPM_PACKAGES/bin"
     && -z $(echo $PATH | grep -o "$NPM_PACKAGES/bin") ]]; then
     # npm global packages without sudo
