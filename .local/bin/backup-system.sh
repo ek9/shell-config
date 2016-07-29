c8afa23d0f3f54a5122387886de8db9fd1d7f970
ek9 <dev@ek9.co>
2016-07-30 01:14:17 +0200

    cleanup for rename to shell-config repo

--- .local/bin/backup-system.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z $1 ] || [ ! -d "$1" ]; then
-	echo "Please provide target directory as an argument"
-	exit 1
-fi
-
-sudo rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} --delete /* $1
--- .local/bin/random-pass.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z $1 ]; then
-    L=32
-else
-    L=$1
-fi
-
-openssl rand -base64 $L
